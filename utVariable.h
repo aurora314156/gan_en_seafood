aa3b0616e40d65ac4581dbbf73141094071ca932
DESKTOP-T7CIDVH\wirl <aurora314156@gmail.com>
2017-10-19 18:25:12 +0800

    hw3thirdpush

--- utVariable.h
+++ utVariable.h
@@ -6,7 +6,9 @@
 #include "struct.h"
 #include "atom.h"
 #include "number.h"
+#include <vector>
 
+using namespace std;
 
 TEST(Variable, constructor){
   Variable X("X");
@@ -96,11 +98,20 @@ TEST (Variable, num1_to_varZ_to_varY_to_varX) {
   ASSERT_EQ("1",Y.value());
   ASSERT_EQ("1",Z.value());
 }
-/*
+
 // ?- X=Y, X=Z, Z=1
 // X=1, Y=1, Z=1
 TEST (Variable, num1_to_varZ_to_varX_and_varY_to_varX) {
-  
+  Variable X("X");
+  Variable Y("Y");
+  Variable Z("Z");
+  Number number (1);
+  X.match(Y);
+  X.match(Z);
+  Z.match(number);
+  ASSERT_EQ("1",X.value());
+  ASSERT_EQ("1",Y.value());
+  ASSERT_EQ("1",Z.value());
 }
 
 // Give there is a Struct s contains Variable X
@@ -109,7 +120,14 @@ TEST (Variable, num1_to_varZ_to_varX_and_varY_to_varX) {
 // Then #symbol() of Y should return "Y"
 // And #value() of Y should return "s(X)"
 TEST (Variable, Struct1) {
-
+  Variable X("X");
+  Variable Y("Y");
+  Atom tom("tom");
+  vector <Term *> v = {&X};
+  Struct s(Atom("s"),v);
+  Y.match(s);
+  ASSERT_EQ("Y",Y.symbol());
+  ASSERT_EQ("s(X)",Y.value());
 }
 
 // Give there is a Struct s contains Variable X
@@ -119,7 +137,15 @@ TEST (Variable, Struct1) {
 // Then #symbol() of Y should return "Y"
 // And #value() of Y should return "s(teddy)"
 TEST (Variable, Struct2) {
-  
-}*/
+  Variable X("X");
+  Variable Y("Y");
+  Atom teddy("teddy");
+  vector <Term *> v = {&X};
+  Struct s(Atom("s"),v);
+  Y.match(s);
+  X.match(teddy);
+  ASSERT_EQ("Y",Y.symbol());
+  ASSERT_EQ("s(teddy)",Y.value());
+}
 
 #endif
