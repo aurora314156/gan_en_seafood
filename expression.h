70c3fb47e53cb46aef79b373f2d8ffb1abcca0ac
DESKTOP-T7CIDVH\wirl <aurora314156@gmail.com>
2018-01-04 22:34:27 +0800

    forfreedom

--- expression.h
+++ expression.h
@@ -211,89 +211,89 @@ TEST(Shell, conjunctionMatching_duplicateExp) {
   }
 }
 
-// TEST(Shell, disjunctionMatching1) {
-//   Scanner s("X=1; X=2.");
-//   Parser p(s);
-//   try {
-//     p.buildExpression();
-//     string result = p.result();
-//     ASSERT_EQ("X = 1; X = 2.", result);
-//   } catch (std::string &msg) {
-//     FAIL() << msg;
-//   }
-// }
+TEST(Shell, disjunctionMatching1) {
+  Scanner s("X=1; X=2.");
+  Parser p(s);
+  try {
+    p.buildExpression();
+    string result = p.result();
+    ASSERT_EQ("X = 1; X = 2.", result);
+  } catch (std::string &msg) {
+    FAIL() << msg;
+  }
+}
 
-// TEST(Shell, disjunctionMatching2) {
-//   Scanner s("X=1; X=1, X=2.");
-//   Parser p(s);
-//   try {
-//     p.buildExpression();
-//     string result = p.result();
-//     ASSERT_EQ("X = 1.", result);
-//   } catch (std::string &msg) {
-//     FAIL() << msg;
-//   }
-// }
+TEST(Shell, disjunctionMatching2) {
+  Scanner s("X=1; X=1, X=2.");
+  Parser p(s);
+  try {
+    p.buildExpression();
+    string result = p.result();
+    ASSERT_EQ("X = 1.", result);
+  } catch (std::string &msg) {
+    FAIL() << msg;
+  }
+}
 
-// TEST(Shell, disjunctionMatching3) {
-//   Scanner s("X=1; X=1, Y=2.");
-//   Parser p(s);
-//   try {
-//     p.buildExpression();
-//     string result = p.result();
-//     ASSERT_EQ("X = 1; X = 1, Y = 2.",result);
-//   } catch (std::string &msg) {
-//     FAIL() << msg;
-//   }
-// }
+TEST(Shell, disjunctionMatching3) {
+  Scanner s("X=1; X=1, Y=2.");
+  Parser p(s);
+  try {
+    p.buildExpression();
+    string result = p.result();
+    ASSERT_EQ("X = 1; X = 1, Y = 2.",result);
+  } catch (std::string &msg) {
+    FAIL() << msg;
+  }
+}
 
-// TEST(Shell, disjunctionMatching4) {
-//   Scanner s("X=1; X=3, X=X.");
-//   Parser p(s);
-//   try {
-//     p.buildExpression();
-//     string result = p.result();
-//     ASSERT_EQ("X = 1; X = 3.", result);
-//   } catch (std::string &msg) {
-//     FAIL() << msg;
-//   }
-// }
+TEST(Shell, disjunctionMatching4) {
+  Scanner s("X=1; X=3, X=X.");
+  Parser p(s);
+  try {
+    p.buildExpression();
+    string result = p.result();
+    ASSERT_EQ("X = 1; X = 3.", result);
+  } catch (std::string &msg) {
+    FAIL() << msg;
+  }
+}
 
-// TEST(Shell, disjunctionMatching5) {
-//   Scanner s("X=1; X=X; Y=2.");
-//   Parser p(s);
-//   try {
-//     p.buildExpression();
-//     string result = p.result();
-//     ASSERT_EQ("X = 1; Y = 2.", result);
-//   } catch (std::string &msg) {
-//     FAIL() << msg;
-//   }
-// }
+TEST(Shell, disjunctionMatching5) {
+  Scanner s("X=1; X=X; Y=2.");
+  Parser p(s);
+  try {
+    p.buildExpression();
+    string result = p.result();
+    ASSERT_EQ("X = 1; Y = 2.", result);
+  } catch (std::string &msg) {
+    FAIL() << msg;
+  }
+}
 
-// TEST(Shell, disjunctionMatching6) {
-//   Scanner s("X=1; X=1, X=2; Z=3.");
-//   Parser p(s);
-//   try {
-//     p.buildExpression();
-//     string result = p.result();
-//     ASSERT_EQ("X = 1; Z = 3.", result);
-//   } catch (std::string &msg) {
-//     FAIL() << msg;
-//   }
-// }
+TEST(Shell, disjunctionMatching6) {
+  Scanner s("X=1; X=1, X=2; Z=3.");
+  Parser p(s);
+  try {
+    p.buildExpression();
+    string result = p.result();
+    ASSERT_EQ("X = 1; Z = 3.", result);
+  } catch (std::string &msg) {
+    FAIL() << msg;
+  }
+}
 
 
-// TEST(Shell, exceptionMissingPeriodToken) {
-//   Scanner s("X=1");
-//   Parser p(s);
-//   try {
-//     p.buildExpression();
-//     // std::string result = p.result();  
-//     FAIL() << "It should throw an exception: Missing token '.'";
-//   } catch (std::string &msg) {
-//     ASSERT_EQ("Missing token '.'", msg);
-//   }
-// }
+TEST(Shell, exceptionMissingPeriodToken) {
+  Scanner s("X=1");
+  Parser p(s);
+  try {
+    p.buildExpression();
+    string result = p.result();  
+    FAIL() << "It should throw an exception: Missing token '.'";
+  } catch (std::string &msg) {
+    ASSERT_EQ("Missing token '.'", msg);
+  }
+}
 
 #endif
